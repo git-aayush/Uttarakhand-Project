7133624ead492de5878d3dd20006721b92cea4bb
Aayush Tyagi <tyagiaayush31@gmail.com>
2024-01-24 17:48:27 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function showSidebar() {
+    const sidebar = document.querySelector('.nav__links__sidebar');
+    sidebar.style.display = "flex";
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector('.nav__links__sidebar');
+    sidebar.style.display = "none";
+}
+
+
+
+let next = document.querySelector('.next')
+let prev = document.querySelector('.prev')
+
+next.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').appendChild(items[0])
+})
+
+prev.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1])
+})
